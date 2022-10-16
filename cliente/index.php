873aca3c4c6b95ff1a7600a9bd1751c892fa6264
Fabricio Diaz <freykill1999@gmail.com>
2022-10-16 15:03:21 -0500

    que es eso

--- cliente/index.php
+++ cliente/index.php
@@ -7,7 +7,7 @@ require_once "controllers/curl.controller.php";
 ini_set('display_erros',1);
 ini_set("log_errors",1);
 ini_set("error_log", "C:/xampp/htdocs/SistemaF/cliente/php_error_log");
-
+//cabios raros
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, Authorization, X-Requested-With, Content-Type, Accept");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
