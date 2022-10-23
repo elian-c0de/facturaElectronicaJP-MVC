9cb19a4323ad01b82a424908f80f129928c1b427
Elian <elianRAMIER2017@hotmail.com>
2022-10-23 11:55:51 -0500

    XD

--- api-rest/config/config.php
+++ api-rest/config/config.php
@@ -1,18 +1,19 @@
 <?php 
     //configuracion de acceso a la base de datos
-    define('DB_HOST', 'DESKTOP-J4VGQP7');
+    define('DB_HOST', 'ELIANGL753VE');
     define('DB_USUARIO', 'sa');
-    define('DB_PASSWORD', 'root123');
+    define('DB_PASSWORD', 'sa1308');
     define('DB_NOMBRE', 'PECMP_JPEREZ');
 
     //Servidor Elian: ELIANGL753VE
+    //Pass Elian: sa1308
     //servidor Miguel: DESKTOP-J4VGQP7
     //$serverName = "DESKTOP-J4VGQP7";
     //DESKTOP-TVN4BC2
     //ruta de la aplicacion
 //    define('RUTA_APP', dirname(dirname(__FILE__)));
 
-//    //Ruta url Ejmplo: http://localhost/fabri_mvc/
+   //Ruta url Ejmplo: http://localhost/fabri_mvc/
 //    define('RUTA_URL', 'http://localhost/fabri_mvc');
 
 //    DEFINE('NOMBRESITIO', 'pepito');
