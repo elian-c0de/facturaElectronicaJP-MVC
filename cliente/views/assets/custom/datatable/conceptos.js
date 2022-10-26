9b91c238ea9ed50f288394288c89a808efd7f21f
Miguel <mpaladine5@utmachala.edu.ec>
2022-10-26 13:28:30 -0500

    Actualizacion Conceptos

--- cliente/views/assets/custom/datatable/conceptos.js
+++ cliente/views/assets/custom/datatable/conceptos.js
@@ -22,6 +22,32 @@ function execDataTable (text) {
          {"data":"sts_concepto"},
          {"data":"actions"}
        ],
+       "language": {
+
+      "sProcessing":     "Procesando...",
+      "sLengthMenu":     "Mostrar _MENU_ Entradas",
+      "sZeroRecords":    "No se encontraron resultados",
+      "sEmptyTable":     "Ningún dato disponible en esta tabla",
+      "sInfo":           "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+      "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0",
+      "sInfoFiltered":   "(filtrado de un total de MAX registros)",
+      "sInfoPostFix":    "",
+      "sSearch":         "Buscar:",
+      "sUrl":            "",
+      "sInfoThousands":  ",",
+      "sLoadingRecords": "Cargando...",
+      "oPaginate": {
+        "sFirst":    "Primero",
+        "sLast":     "Último",
+        "sNext":     "Siguiente",
+        "sPrevious": "Anterior"
+      },
+      "oAria": {
+        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+      }
+
+    },
        "buttons": [
          {extend:"copy",className:"btn-dark"},
          {extend:"csv",className:"btn-b"},
