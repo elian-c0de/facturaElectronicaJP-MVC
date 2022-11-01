afdfc0549a866a50ae90f6dfabc76001e376d8ec
Elian <elianRAMIER2017@hotmail.com>
2022-11-01 16:59:46 -0500

    PUNTO EMISION v2

--- cliente/views/assets/custom/forms/forms.js
+++ cliente/views/assets/custom/forms/forms.js
@@ -176,8 +176,8 @@ function validateJS(event, type) {
   if(type == "por_reten") pattern = /^[0-9]{1,3}([.][0-9]{1,2})?$/;
   if(type == "nom_usuario") pattern = /^[A-Za-zñÑáéíóúÁÉÍÓÚ ]{1,50}$/;
   if(type == "cod_usuario") pattern = /^[a-zñÑáéíóúÁÉÍÓÚ ]{1,20}$/;
-  if(type == "num_factura") pattern = /^[0-9]{1,9}$/;
   if(type == "cod_punto_emision") pattern = /^[a-zA-Z0-9]{1,3}$/;
+  if(type == "num_factura") pattern = /^[0-9]{1,9}$/;
 
   if (!pattern.test(event.target.value)) {
  
