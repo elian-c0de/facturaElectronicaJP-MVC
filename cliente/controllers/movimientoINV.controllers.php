fbf135ee5cc8f75b06bfda974a5217ef9c4c3e31
Miguel <mpaladine5@utmachala.edu.ec>
2022-11-18 03:22:02 -0500

    Actualizacion en movimientoINV.PHP

--- cliente/controllers/movimientoINV.controllers.php
+++ cliente/controllers/movimientoINV.controllers.php
@@ -1,34 +1,63 @@
 <?php
 
-require_once ("../controllers/curl.controller.php");
+require_once("../controllers/curl.controller.php");
 
-class CreateController{
+class CreateController
+{
 
-   public $array;
-   
-   public $cod_empresa;
-   public $cod_establecimiento;
-   public $num_documento;
-   public $cod_documento;
-   public $token;
-   public $fec_actualiza;
-   public $txt_descripcion;
-   public $cod_usuario;
-   
-    
- 
+    public $array;
 
-    public function dataCreate(){
-        // $variable = $this->array;
-        // foreach ($variable as $key => $value) {
-        //     list($valor1, $valor2, $valor3, $valor4, $valor5, $valor6, $valor7) = $variable;
-        // echo "Valor1=$value Valor2=$value Valor3=$value Valor4=$value Valor5=$value Valor6=$value Valor7=$value";
-        // return;
-             
+    public $cod_empresa;
+    public $cod_establecimiento;
+    public $num_documento;
+    public $cod_documento;
+    public $token;
+    public $fec_actualiza;
+    public $txt_descripcion;
+    public $cod_usuario;
+
+
+
+
+    public function dataCreate()
+    {
+
+        $variable = $this->array;
+
+
+        // //saco el numero de elementos
+        // $longitud = count($variable);
+
+        // //Recorro todos los elementos
+        // for ($i = 0; $i < $longitud; $i++) {
+        //     //saco el valor de cada elemento
+        //     // echo $variable[$i];
+        //     echo '<pre>'; print_r($variable[$i]); echo '</pre>';
+
         // }
-        
-        // $data1 = array(
+        // return;
+        $array = [];
+        foreach ($variable as $clave=> $value) {
+            // $var = $value;
+            // echo '</pre>';
+            // print_r($var);
+            // echo '</pre>';
+            //echo "   {$value} ";
+            $array['cod_inventario'] = $value;
+            var_dump( $array);
+            // print_r($variable);
             
+
+            // for ($i=0; $i < $clave; $i++) { 
+            //     echo "{$clave} => {$value} ";
+            // }
+            
+        }
+
+        return;
+
+        // $data1 = array(
+
 
         //     "cod_empresa" => $_SESSION["admin"]->cod_empresa,
         //     "cod_establecimiento" => trim($variable[1]),
@@ -42,69 +71,64 @@ class CreateController{
         // echo '<pre>'; print_r($data1); echo '</pre>';
         // return;
 
-         $data = array(
-                
+        $data = array(
 
-                "cod_empresa" => trim($this->cod_empresa),
-                "cod_establecimiento" => trim($this->cod_establecimiento),
-                "num_documento" => trim($this->num_documento),
-                "cod_documento" => trim($this->cod_documento),
-                "fec_documento" => trim($this->fec_actualiza),
-                "txt_observacion" => trim($this->txt_descripcion),
-                "cod_usuario" => trim($this->cod_usuario),
-                "fec_actualiza" => trim($this->fec_actualiza),
-                "sts_cabecera_inventario" => "A"
 
-            );
-            
-            echo '<pre>'; print_r( $data ); echo '</pre>';
+            "cod_empresa" => trim($this->cod_empresa),
+            "cod_establecimiento" => trim($this->cod_establecimiento),
+            "num_documento" => trim($this->num_documento),
+            "cod_documento" => trim($this->cod_documento),
+            "fec_documento" => trim($this->fec_actualiza),
+            "txt_observacion" => trim($this->txt_descripcion),
+            "cod_usuario" => trim($this->cod_usuario),
+            "fec_actualiza" => trim($this->fec_actualiza),
+            "sts_cabecera_inventario" => "A"
+
+        );
+
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
 
         //  $url = "ecmp_detalle_inventario?token=".$this->token;
         //  $method = "POST";
         //  $fields = $data;
         //  $response = CurlController::request($url,$method,$fields);
-          
-            
-       
+
+
+
         //     if($response == null){
         //         echo "400";
         //     }else{
         //         echo $response->status;
-    
+
         //     }
 
         // }else{
         //     echo 404;
         // }
 
-       
 
+
+    }
 }
 
 
+if (isset($_POST["cod_establecimiento"])) {
 
 
+    $validate = new CreateController();
 
+    $validate->array = $_POST["array"];
 
-}
+    $validate->cod_empresa = $_POST["cod_empresa"];
+    $validate->cod_establecimiento = $_POST["cod_establecimiento"];
+    $validate->token = $_POST["token"];
+    $validate->num_documento = $_POST["num_documento"];
+    $validate->cod_documento = $_POST["cod_documento"];
+    $validate->fec_actualiza = $_POST["fec_actualiza"];
+    $validate->txt_descripcion = $_POST["txt_descripcion"];
+    $validate->cod_usuario = $_POST["cod_usuario"];
 
-
-if(isset($_POST["cod_establecimiento"])){
-    
-
-$validate = new CreateController();
-
- $validate -> array = $_POST["array"];
-
- $validate -> cod_empresa = $_POST["cod_empresa"];
- $validate -> cod_establecimiento = $_POST["cod_establecimiento"];
- $validate -> token = $_POST["token"];
- $validate -> num_documento = $_POST["num_documento"];
- $validate -> cod_documento = $_POST["cod_documento"];
- $validate -> fec_actualiza = $_POST["fec_actualiza"];
- $validate -> txt_descripcion = $_POST["txt_descripcion"];
- $validate -> cod_usuario = $_POST["cod_usuario"];
-
- $validate->dataCreate();
-
+    $validate->dataCreate();
 }
